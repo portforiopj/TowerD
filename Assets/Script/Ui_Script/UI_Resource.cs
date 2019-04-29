7844deed726abbe1fd5a15497d0fb9b3c0954c0c
limsk51 <limsk51@naver.com>
2019-04-29 16:47:27 +0900

    04-29 UI work
    UI Panel work (not yet control panel)

---
+++ Assets/Script/Ui_Script/UI_Resource.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Resource : MonoBehaviour
+{
+
+    //선택한 자원 표시 이펙트
+    public GameObject UIres_Effet;
+
+    //자원 클릭 시 이펙트 이동 시킬 transform 값
+    public Transform UIres_RedTr;
+    public Transform UIres_BlueTr;
+    public Transform UIres_GreenTr;
+
+    //현재 선택한 자원 생성 값
+    public string UIres_CurrentResource;
+
+
+    void Start()
+    {
+        UIres_Effet.transform.position = UIres_RedTr.position;  // Effect의 transform Default 값
+        UIres_CurrentResource = "Red";  // 생성하는 자원 변경에 쓰일 string  Default 값
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    public void ClickRed()  //빨간거 선택
+    {
+        UIres_Effet.transform.position = UIres_RedTr.position;  //transform 값으로 이동
+        UIres_CurrentResource = "Red";  // 생성하는 자원 변경에 쓰일 string 값 변경
+
+        //test
+        Debug.Log(UIres_CurrentResource);
+    }
+
+    public void ClickBlue() //파란거 선택
+    {
+        UIres_Effet.transform.position = UIres_BlueTr.position;
+        UIres_CurrentResource = "Blue";
+
+        //test
+        Debug.Log(UIres_CurrentResource);
+    }
+
+    public void ClickGreen()    //초록색 선택
+    {
+        UIres_Effet.transform.position = UIres_GreenTr.position;
+        UIres_CurrentResource = "Green";
+
+        //test
+        Debug.Log(UIres_CurrentResource);
+    }
+}
---
+++ Assets/Script/Ui_Script/UI_StageNum.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_StageNum : MonoBehaviour
+{
+    //test 용 정보값
+    int StageNum = 1;
+    int CurrentWave = 1;
+    int WaveNum = 6;
+
+    public Text UIstg_stgnum;
+    public Text UIstg_wavenum;
+
+
+    void Start()
+    {
+        SetStgnum();
+        SetWavenum();
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    public void SetStgnum()
+    {
+        UIstg_stgnum.text = StageNum.ToString();
+    }
+
+    public void SetWavenum()
+    {
+        UIstg_wavenum.text = CurrentWave + " / " + WaveNum;
+    }
+}
